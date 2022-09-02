2ba4293f8c9965c55e5997aeded0bbccc730715f
DominikZurawski <38438473+DominikZurawski@users.noreply.github.com>
2022-09-02 15:01:57 +0200

    Update patron.py

--- flask/app/api/patron.py
+++ flask/app/api/patron.py
@@ -65,9 +65,9 @@ class PatronDrawApi(Resource):
         #app.logger.info(dicts[0]['patron'])
         aggregate =  sample(list(dicts), k=daysInMonth)
         #app.logger.info(agregate)
-        list(dicts) = aggregate
+        dicts[0]['quotes'] = aggregate
 
-        return list(dicts)
+        return dicts  
 
 class PatronDownloadImage(Resource):
 
